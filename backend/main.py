b9ec4e1d9aaca7a947f9932933dfc8e229137602
Md. Zihad Bin Jahangir <86883679+zihadbinjahangir@users.noreply.github.com>
2025-05-24 00:03:37 +0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
-from utils import load_model, predict_and_mask
+from utils import load_model, predict_and_mask, download_model
 from PIL import Image
 import io
 
 app = FastAPI()
+download_model()
 model = load_model("polygon_model.pth")
 
 @app.post("/predict/")
